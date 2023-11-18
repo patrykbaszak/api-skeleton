460a899d4a5aa3ed9f5673f2a08ae92d4028811f
patrykbaszak <patryk.baszak@gmail.com>
2023-11-18 15:00:59 +0100

    Added one more package

--- scripts/Setup.php
+++ scripts/Setup.php
@@ -6,14 +6,30 @@ use Symfony\Component\Yaml\Yaml;
 
 require_once dirname(__DIR__).'/vendor/autoload.php';
 
-// setup controller
+$start = file_get_contents(__DIR__ . '/../skeleton/start.sh');
+$composer  = json_decode(file_get_contents(__DIR__ . '/../skeleton/composer.json'));
 $routes = Yaml::parse(file_get_contents(__DIR__ . '/../skeleton/config/routes.yaml'));
+
+// setup composer
+$composer->title ??= 'API';
+$composer->description ??= 'Awesome API';
+
+// setup controller
 $routes['controllers']['resource']['path'] = '../src/Presentation/Controller/API/';
 $routes['controllers']['resource']['namespace'] = 'App\Presentation\Controller\API';
-file_put_contents(__DIR__ . '/../skeleton/config/routes.yaml', Yaml::dump($routes, 10, 4));
+
+// setup nelmio
+$routes['extended_api_doc']['resource'] = '@DocumentationBundle/Resources/routes/*';
+$composer->repositories = [
+    (object) [
+        'type' => 'vcs',
+        'url' => 'https://github.com/DjordyKoert/NelmioApiDocBundle.git'
+    ]
+];
+$composer->require->{'nelmio/api-doc-bundle'} = 'dev-symfony-map-request-data';
+$composer->extra->symfony->{'allow-contrib'} = true;
 
 // setup start script
-$start = file_get_contents(__DIR__ . '/../skeleton/start.sh');
 $search = 'docker run -d --name php \
     --env-file .env.local \
     -v $(pwd):/app \
@@ -26,4 +42,7 @@ $replace = 'docker run -d --name php \
     -p 8080:8080 \
     $IMAGE_NAME php -S 0.0.0.0:8080 -t public';
 $start = str_replace($search, $replace, $start);
+
 file_put_contents(__DIR__ . '/../skeleton/start.sh', $start);
+file_put_contents(__DIR__ . '/../skeleton/config/routes.yaml', Yaml::dump($routes, 10, 4));
+file_put_contents(__DIR__ . '/../skeleton/composer.json', json_encode($composer, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
--- start.sh
+++ start.sh
@@ -12,7 +12,7 @@ docker run --rm -v $(pwd):/app -w /app php:latest php scripts/Setup.php
 # ApiDoc Bundle
 cp src/config/routes/nelmio_api_doc.yaml skeleton/config/routes/nelmio_api_doc.yaml
 cp src/config/packages/nelmio_api_doc.yaml skeleton/config/packages/nelmio_api_doc.yaml
-docker run --rm -v $(pwd):/app -w /app php:latest php scripts/AddNelmioApiDocBundle.php
+docker run --rm -v $(pwd)/skeleton:/app -w /app composer:latest composer require pbaszak/extended-api-doc-bundle
 
 rm -rf node_modules scripts src .gitignore CHANGELOG.md composer.json composer.lock README.md LICENSE .git vendor start.sh package.json package-lock.json
 
