99208d1e22509b6f990deb8944a0856608609902
patrykbaszak <patryk.baszak@gmail.com>
2023-11-18 16:09:50 +0100

    fix: Changed

--- scripts/Setup.php
+++ scripts/Setup.php
@@ -9,6 +9,7 @@ require_once dirname(__DIR__).'/vendor/autoload.php';
 $start = file_get_contents(__DIR__ . '/../skeleton/start.sh');
 $composer  = json_decode(file_get_contents(__DIR__ . '/../skeleton/composer.json'));
 $routes = Yaml::parse(file_get_contents(__DIR__ . '/../skeleton/config/routes.yaml'));
+$bundles = include __DIR__ . '/../skeleton/config/bundles.php';
 
 // setup composer
 $composer->title ??= 'API';
@@ -27,7 +28,18 @@ $composer->repositories = [
     ]
 ];
 $composer->require->{'nelmio/api-doc-bundle'} = 'dev-symfony-map-request-data';
+$composer->require->{'pbaszak/extended-api-doc-bundle'} = '^1.1';
 $composer->extra->symfony->{'allow-contrib'} = true;
+$bundles[Nelmio\ApiDocBundle\NelmioApiDocBundle::class] = ['all' => true];
+$bundles[PBaszak\ExtendedApiDoc\ExtendedApiDocBundle::class] = ['all' => true];
+
+// setup twig (required for nelmio)
+$composer->require->{'symfony/asset'} = '^6.3';
+$composer->require->{'symfony/twig-bundle'} = '^6.3';
+$composer->require->{'twig/extra-bundle'} = '^3.0';
+$composer->require->{'twig/twig'} = '^3.0';
+$bundles[Symfony\Bundle\TwigBundle\TwigBundle::class] = ['all' => true];
+$bundles[Twig\Extra\TwigExtraBundle\TwigExtraBundle::class] = ['all' => true];
 
 // setup start script
 $search = 'docker run -d --name php \
@@ -46,3 +58,4 @@ $start = str_replace($search, $replace, $start);
 file_put_contents(__DIR__ . '/../skeleton/start.sh', $start);
 file_put_contents(__DIR__ . '/../skeleton/config/routes.yaml', Yaml::dump($routes, 10, 4));
 file_put_contents(__DIR__ . '/../skeleton/composer.json', json_encode($composer, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+file_put_contents(__DIR__ . '/../skeleton/config/bundles.php', '<?php' . PHP_EOL . PHP_EOL . 'return ' . var_export($bundles, true) . ';' . PHP_EOL);
--- start.sh
+++ start.sh
@@ -2,23 +2,18 @@
 
 composer create-project pbaszak/skeleton --no-interaction
 
-sudo rm -rf skeleton/src
-sudo cp -r src/src skeleton/src
-sudo cp src/Dockerfile skeleton/Dockerfile
+rm -rf skeleton/src
+cp -r src/src skeleton/src
 
-docker run --rm -v $(pwd)/skeleton:/app -w /app composer:latest composer require twig asset --no-interaction
 docker run --rm -v $(pwd):/app -w /app php:latest php scripts/Setup.php
 
 # ApiDoc Bundle
-sudo cp src/config/routes/nelmio_api_doc.yaml skeleton/config/routes/nelmio_api_doc.yaml
-sudo cp src/config/packages/nelmio_api_doc.yaml skeleton/config/packages/nelmio_api_doc.yaml
-docker run --rm -v $(pwd)/skeleton:/app -w /app composer:latest composer require pbaszak/extended-api-doc-bundle
+cp src/config/routes/nelmio_api_doc.yaml skeleton/config/routes/nelmio_api_doc.yaml
+cp src/config/packages/nelmio_api_doc.yaml skeleton/config/packages/nelmio_api_doc.yaml
 
-sudo rm -rf node_modules scripts src .gitignore CHANGELOG.md composer.json composer.lock README.md LICENSE .git vendor start.sh package.json package-lock.json
-
-sudo mv skeleton/{,.[^.]}* ./
-
-sudo rm -rf skeleton
+rm -rf node_modules scripts src .gitignore CHANGELOG.md composer.json composer.lock README.md LICENSE .git vendor start.sh package.json package-lock.json
+mv skeleton/{,.[^.]}* ./
+rm -rf skeleton
 
 bash start.sh
-docker exec php composer update --no-interaction || composer update --no-interaction
+docker exec php composer update || composer update
