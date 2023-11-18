34abd0f93871bb70a6e7a07c03d0819d92df4ec0
patrykbaszak <patryk.baszak@gmail.com>
2023-11-18 15:09:31 +0100

    fix: changed routes

--- start.sh
+++ start.sh
@@ -2,23 +2,23 @@
 
 composer create-project pbaszak/skeleton --no-interaction
 
-rm -rf skeleton/src
-cp -r src/src skeleton/src
-cp src/Dockerfile skeleton/Dockerfile
+sudo rm -rf skeleton/src
+sudo cp -r src/src skeleton/src
+sudo cp src/Dockerfile skeleton/Dockerfile
 
 docker run --rm -v $(pwd)/skeleton:/app -w /app composer:latest composer require twig asset --no-interaction
 docker run --rm -v $(pwd):/app -w /app php:latest php scripts/Setup.php
 
 # ApiDoc Bundle
-cp src/config/routes/nelmio_api_doc.yaml skeleton/config/routes/nelmio_api_doc.yaml
-cp src/config/packages/nelmio_api_doc.yaml skeleton/config/packages/nelmio_api_doc.yaml
+sudo cp src/config/routes/nelmio_api_doc.yaml skeleton/config/routes/nelmio_api_doc.yaml
+sudo cp src/config/packages/nelmio_api_doc.yaml skeleton/config/packages/nelmio_api_doc.yaml
 docker run --rm -v $(pwd)/skeleton:/app -w /app composer:latest composer require pbaszak/extended-api-doc-bundle
 
-rm -rf node_modules scripts src .gitignore CHANGELOG.md composer.json composer.lock README.md LICENSE .git vendor start.sh package.json package-lock.json
+sudo rm -rf node_modules scripts src .gitignore CHANGELOG.md composer.json composer.lock README.md LICENSE .git vendor start.sh package.json package-lock.json
 
-mv skeleton/{,.[^.]}* ./
+sudo mv skeleton/{,.[^.]}* ./
 
-rm -rf skeleton
+sudo rm -rf skeleton
 
 bash start.sh
 docker exec php composer update --no-interaction || composer update --no-interaction
